70dc3d3a8d31ec8db671399f92588b9a22ecc3a1
Tausif Islam Sheik <mr.tausif904@gmail.com>
2026-03-16 16:45:45 +0600

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/shared/Footer";
 import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
